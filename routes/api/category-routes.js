7646f3b8fee3c6a02d0bfe537a7d675af6505bf0
Brandon <beegee54321@gmail.com>
2024-01-20 20:17:56 -0800

    adding to routes saving progress

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+
   try {
     const categoryData = await Category.findAll(
-      { include : [{ model: Product }]
+      { include : [{ 
+        model: Product }]
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -17,11 +15,11 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }]
+      include: [{ 
+        model: Product }]
     });
     if (!categoryData) {
       res.status(404).json({ 
@@ -36,7 +34,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
+
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -46,7 +44,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+ 
   try {
     const categoryData = await Category.update(req.body, {
       where: {
@@ -60,7 +58,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+
   try {
     const categoryData = await Category.destroy({
       where: {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,7 +3,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
 });
